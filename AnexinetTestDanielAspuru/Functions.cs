5443a089f96863005000b29808c7f990b17bc9fa
Daniel Aspuru <daspuru@gmail.com>
2019-11-26 14:23:05 -0600

    Question 1 solved

---
+++ AnexinetTestDanielAspuru/Functions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AnexinetTestDanielAspuru
+{
+    //This class includes all the functions requested in the test description, there is a client
+    //caller to run all tests in the TestRunner class.
+    public static class Functions
+    {
+        //1.- Write a function that receives a String parameter and returns how many vowels that string has.
+        public static int CountVowels(string input)
+        {
+            //Initialize counter
+            int totalVowels = 0;
+
+            //Define an array with the vowels (with and without accents)
+            char[] vowels = { 'A', 'E', 'I', 'O', 'U', 'Á', 'É', 'Ó', 'Ú' };
+
+            //Split the string with the char as separator and count the number of parts minus one
+            foreach (char vowel in vowels)
+            {
+                totalVowels += input.ToUpper().Split(vowel).Length - 1;
+            }
+
+            //Return the counter
+            return totalVowels;
+        }
+
+        
+    }
+}
---
+++ AnexinetTestDanielAspuru/TestRunner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace AnexinetTestDanielAspuru
+{
+    //Using the method RunTest() will run all test. This method is already called in the App.xaml.cs file.
+    //You can just run the app too see test results.
+    public static class TestRunner
+    {
+        public static void RunTest()
+        {
+            //Start Test with output
+
+            //1.- Write a function that receives a String parameter and returns how many vowels that string has.
+            string stringToCountVowels = "¿Cuántas vocales tengo?"; //This string should count the 8 contained vowels
+
+            int numberOfVowles = Functions.CountVowels(stringToCountVowels);
+
+            Console.WriteLine("String: \"" + stringToCountVowels + "\" has " +  numberOfVowles.ToString() + " vowels");
+
+        }
+    }
+}
